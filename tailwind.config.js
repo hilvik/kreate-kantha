e7b4c45c283c2db6df178d12c11b823425549aff
Sahil Vikas <sahilvikassv@gmail.com>
2025-05-12 23:28:23 +0530

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        black: '#000000',
+        white: '#FFFFFF',
+        lightGray: '#E5E7EB',
+        pink: '#D2877D',
+        darkGray: '#262626',
+        rust: '#C0392B',
+        lightBlue: '#CED4DA',
+        mediumGray: '#D4D4D4',
+        lightBeige: '#EBE8E5',
+        cream: '#F2E9DA',
+        lightestGray: '#F5F5F5'
+      },
+    },
+    fontFamily: {
+      'sans': ['Open Sans', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
